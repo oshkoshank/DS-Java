70dc030af162f41eff0b4bea356aee55d3226f2c
oshkoshank <bandstars45@gmail.com>
2022-10-17 14:08:21 +0530

    commut

---
+++ Stack/Stacks.java
@@ -0,0 +1,35 @@
+package Stack;
+import java.net.SocketTimeoutException;
+import java.util.*;
+public class Stacks 
+{
+    public static void main(String[] args) 
+    {
+        Scanner o = new Scanner(System.in);
+        System.out.print("Stack Implementation using Array: ");
+        boolean flag = true;
+        System.out.print("Enter the stack size: ");
+        int sz = o.nextInt();
+        Stacks stc = new Stacks(sz);
+        while (flag) 
+        {
+            System.out.println("1] Push an Element");
+            System.out.println("2] Pop an Element");
+            System.out.println("3] Display");
+            System.out.println("4] Exit");  
+            System.out.println("Select an option from above ");
+            int ch = o.nextInt();
+            
+            switch (ch) {
+                case 1: System.out.println("Enter the element to push");
+                    int e = o.nextInt();
+                                        
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        o.close();
+    }
+}
